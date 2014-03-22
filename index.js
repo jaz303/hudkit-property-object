4c0a2ea7d08f6c78a7927bb09a1065f9ad9ced83
Jason Frame <jason@onehackoranother.com>
2014-03-22 09:46:07 +0000

    tabs -> spaces

--- index.js
+++ index.js
@@ -1,111 +1,111 @@
 var signal = require('signalkit');
 
 module.exports = function() {
-	return new PropertyObject();
+    return new PropertyObject();
 }
 
 function PropertyObject() {
-	
-	this._values 		= {};
-	this._groups 		= [];
-	this._properties	= {};
+    
+    this._values        = {};
+    this._groups        = [];
+    this._properties    = {};
 
-	// onChange is exposed for convenience only, the official interface
-	// (for use in a PropertyEditor widget) is onPropertyChange(fn)
-	this.onChange = signal('onChange');
+    // onChange is exposed for convenience only, the official interface
+    // (for use in a PropertyEditor widget) is onPropertyChange(fn)
+    this.onChange = signal('onChange');
 
 }
 
 PropertyObject.prototype.group = function(title) {
 
-	var po 		= this,
-		gix		= this._groups.length,
-		names 	= [];
+    var po      = this,
+        gix     = this._groups.length,
+        names   = [];
 
-	var group = {
-		title: title,
-		propertyNames: names,
-		add: function(name, type, initialValue, options) {
+    var group = {
+        title: title,
+        propertyNames: names,
+        add: function(name, type, initialValue, options) {
 
-			if (name in po._properties) {
-				throw new Error("duplicate property: " + name);
-			}
+            if (name in po._properties) {
+                throw new Error("duplicate property: " + name);
+            }
 
-			options = options || {};
-			options.type = type;
+            options = options || {};
+            options.type = type;
 
-			if (!('caption' in options)) {
-				// TODO: humanize name
-				options.caption = name;
-			}
+            if (!('caption' in options)) {
+                // TODO: humanize name
+                options.caption = name;
+            }
 
-			names.push(name);
-			po._values[name] = initialValue;
-			po._properties[name] = options;
+            names.push(name);
+            po._values[name] = initialValue;
+            po._properties[name] = options;
 
-			return this;
+            return this;
 
-		},
-		back: function() {
-			return po;
-		}
-	};
+        },
+        back: function() {
+            return po;
+        }
+    };
 
-	this._groups.push(group);
+    this._groups.push(group);
 
-	return group;
+    return group;
 
 }
 
 PropertyObject.prototype.getPropertyGroupCount = function() {
-	return this._groups.length;
+    return this._groups.length;
 }
 
 PropertyObject.prototype.getPropertyGroupTitle = function(groupIx) {
-	return this._groups[groupIx].title;
+    return this._groups[groupIx].title;
 }
 
 PropertyObject.prototype.getPropertyNames = function(groupIx) {
-	return this._groups[groupIx].propertyNames;
+    return this._groups[groupIx].propertyNames;
 }
 
 PropertyObject.prototype.getPropertyDescriptor = function(name) {
-	return this._properties[name];
+    return this._properties[name];
 }
 
 PropertyObject.prototype.getPropertyValue = function(key) {
 
-	if (!(key in this._values)) {
-		throw new Error("no such property: " + key);
-	}
+    if (!(key in this._values)) {
+        throw new Error("no such property: " + key);
+    }
 
-	return this._values[key];
+    return this._values[key];
 
 }
 
 PropertyObject.prototype.setPropertyValue = function(key, newValue) {
 
-	if (!(key in this._values)) {
-		throw new Error("no such property: " + key);
-	}
+    if (!(key in this._values)) {
+        throw new Error("no such property: " + key);
+    }
 
-	var oldValue = this._values[key];
-	
-	this._values[key] = newValue;
-	
-	this.onChange.emit(this, {
-		property 	: key,
-		oldValue	: oldValue,
-		newValue	: newValue,
-		values 		: this._values
-	});
+    var oldValue = this._values[key];
+    
+    this._values[key] = newValue;
+    
+    this.onChange.emit(this, {
+        property    : key,
+        oldValue    : oldValue,
+        newValue    : newValue,
+        values      : this._values
+    });
 
-	return true;
+    return true;
 
 }
 
 PropertyObject.prototype.onPropertyChange = function(cb) {
-	return this.onChange.connect(cb);
+    return this.onChange.connect(cb);
 }
 
 // again, these are just convenience methods for the end-user; not part
--- test/all.js
+++ test/all.js
@@ -1,114 +1,114 @@
-var po		= require('../'),
-	test	= require('tape');
+var po      = require('../'),
+    test    = require('tape');
 
 function makeTestObject() {
 
-	var obj = po();
+    var obj = po();
 
-	obj.group('G1')
-			.add('foo', 'text', 10)
-			.add('bar', 'checkbox', false, {caption: 'Bar'})
-			.back()
-		.group('G2')
-			.add('baz', 'slider', 50, {a: 1, b: 2, c: 3});
+    obj.group('G1')
+            .add('foo', 'text', 10)
+            .add('bar', 'checkbox', false, {caption: 'Bar'})
+            .back()
+        .group('G2')
+            .add('baz', 'slider', 50, {a: 1, b: 2, c: 3});
 
-	return obj;
+    return obj;
 
 }
 
 test('#getPropertyGroupCount', function(a) {
-	var obj = makeTestObject();
-	a.equal(2, obj.getPropertyGroupCount());
-	a.end();
+    var obj = makeTestObject();
+    a.equal(2, obj.getPropertyGroupCount());
+    a.end();
 });
 
 test('#getPropertyGroupTitle', function(a) {
-	var obj = makeTestObject();
-	a.equal('G1', obj.getPropertyGroupTitle(0));
-	a.equal('G2', obj.getPropertyGroupTitle(1));
-	a.end();
+    var obj = makeTestObject();
+    a.equal('G1', obj.getPropertyGroupTitle(0));
+    a.equal('G2', obj.getPropertyGroupTitle(1));
+    a.end();
 });
 
 test('#getPropertyNames', function(a) {
-	var obj = makeTestObject();
-	a.deepEqual(['foo','bar'], obj.getPropertyNames(0));
-	a.deepEqual(['baz'], obj.getPropertyNames(1));
-	a.end();
+    var obj = makeTestObject();
+    a.deepEqual(['foo','bar'], obj.getPropertyNames(0));
+    a.deepEqual(['baz'], obj.getPropertyNames(1));
+    a.end();
 });
 
 test('#getPropertyDescriptor', function(a) {
-	var obj = makeTestObject();
-	a.deepEqual(['foo','bar'], obj.getPropertyNames(0));
-	a.deepEqual({
-		type: 'slider',
-		a: 1,
-		b: 2,
-		c: 3,
-		caption: 'baz'
-	}, obj.getPropertyDescriptor('baz'));
-	a.end();
+    var obj = makeTestObject();
+    a.deepEqual(['foo','bar'], obj.getPropertyNames(0));
+    a.deepEqual({
+        type: 'slider',
+        a: 1,
+        b: 2,
+        c: 3,
+        caption: 'baz'
+    }, obj.getPropertyDescriptor('baz'));
+    a.end();
 });
 
 test('#getPropertyValue', function(a) {
-	var obj = makeTestObject();
-	a.equal(10, obj.getPropertyValue('foo'));
-	a.equal(false, obj.getPropertyValue('bar'));
-	a.equal(50, obj.getPropertyValue('baz'));
-	a.end();
+    var obj = makeTestObject();
+    a.equal(10, obj.getPropertyValue('foo'));
+    a.equal(false, obj.getPropertyValue('bar'));
+    a.equal(50, obj.getPropertyValue('baz'));
+    a.end();
 });
 
 test('#setPropertyValue', function(a) {
-	var obj = makeTestObject();
-	obj.setPropertyValue('foo', 153);
-	a.equal(153, obj.getPropertyValue('foo'));
-	a.end();
+    var obj = makeTestObject();
+    obj.setPropertyValue('foo', 153);
+    a.equal(153, obj.getPropertyValue('foo'));
+    a.end();
 });
 
 test("duplicate property", function(a) {
-	try {
-		var obj = makeTestObject();
-		obj.group().add('foo', 'text', 30);
-		a.ok(false);
-	} catch (e) {
-		a.ok(true);
-	}
-	a.end();
+    try {
+        var obj = makeTestObject();
+        obj.group().add('foo', 'text', 30);
+        a.ok(false);
+    } catch (e) {
+        a.ok(true);
+    }
+    a.end();
 });
 
 test('#getPropertyValue - missing', function(a) {
-	try {
-		var obj = makeTestObject();
-		obj.getPropertyValue('!!!');
-		a.ok(false);
-	} catch (e) {
-		a.ok(true);
-	}
-	a.end();
+    try {
+        var obj = makeTestObject();
+        obj.getPropertyValue('!!!');
+        a.ok(false);
+    } catch (e) {
+        a.ok(true);
+    }
+    a.end();
 });
 
 test('#setPropertyValue - missing', function(a) {
-	try {
-		var obj = makeTestObject();
-		obj.setPropertyValue('!!!', Infinity);
-		a.ok(false);
-	} catch (e) {
-		a.ok(true);
-	}
-	a.end();
+    try {
+        var obj = makeTestObject();
+        obj.setPropertyValue('!!!', Infinity);
+        a.ok(false);
+    } catch (e) {
+        a.ok(true);
+    }
+    a.end();
 });
 
 test('#onPropertyChange', function(a) {
 
-	var obj = makeTestObject();
-	obj.onPropertyChange(function(src, evt) {
-		a.equal(src, obj);
-		a.equal(evt.property, 'foo'),
-		a.equal(evt.oldValue, 10);
-		a.equal(evt.newValue, 50);
-		a.equal(evt.values, obj._values); // cheeky
-		a.end();
-	});
+    var obj = makeTestObject();
+    obj.onPropertyChange(function(src, evt) {
+        a.equal(src, obj);
+        a.equal(evt.property, 'foo'),
+        a.equal(evt.oldValue, 10);
+        a.equal(evt.newValue, 50);
+        a.equal(evt.values, obj._values); // cheeky
+        a.end();
+    });
 
-	obj.set('foo', 50);
+    obj.set('foo', 50);
 
 });
